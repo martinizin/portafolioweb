1a556a567eefb8a284c5d6e90888f2a0f52e613f
martinizin <mar45pillacela@gmail.com>
2023-12-19 23:09:26 -0500

    modelos y esquemas

---
+++ src/models/Portfolio.js
@@ -0,0 +1,24 @@
+
+//Importación del esquema y el modelo
+const {Schema, model} = require('mongoose')
+
+//Creación del esquema de la tabla de la base de datos
+const portfolioSchema = new Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    description:{
+        type:String,
+        require:true
+    },
+    category :{
+        type:String,
+        require:true
+    }
+},{
+    timestamps:true
+})
+
+//Exportación del modelo y el esquema
+module.exports = model('portfolio',portfolioSchema)
---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+//Importar el esquema y el modelo
+
+const {Schema, model} = require('mongoose')
+const bcrypt = require('bcryptjs')
+const userSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password :{
+        type:String,
+        require:true
+    }
+},{
+    timestamps:true
+})
+
+// Método para cifrar el password del usuario
+userSchema.methods.encrypPassword = async (password)=>{
+    const salt = await bcrypt.genSalt(10)
+    const passwordEncryp = await bcrypt.hash(password,salt)
+    return passwordEncryp
+}
+
+// Método para verificar si el password ingresado es el mismo de la BDD
+userSchema.methods.matchPassword = async function(password){
+    const response = await bcrypt.compare(password,this.password)
+    return response
+}
+
+//Exportación del modelo y el esquema
+module.exports = model('user',userSchema)
