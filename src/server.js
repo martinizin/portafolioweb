fa10c2cf5c6e16a374cefada0d1efc7960a25015
martinizin <mar45pillacela@gmail.com>
2023-12-19 23:30:05 -0500

    Motor de plantillas Handlebars

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const express = require('express')
 //Importamos el path
 const path = require('path');
 
+//Importamos handlebars
+const { engine }  = require('express-handlebars')
 // Inicializaciones
 const app = express()
 
@@ -11,6 +13,21 @@ const app = express()
 app.set('port',process.env.port || 3000)
 app.set('views',path.join(__dirname, 'views'))
 
+//Establecemos el directorio de las vistas
+app.set('views',path.join(__dirname, 'views'))
+//Configuración para el botón de plantilla
+//1 Archivo master (Master page)
+//2 Establecemos el directorio layouts
+//3 Establecemos el directorio partials
+//4 Extensión de las páginas (.hbs)
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+//Establecemos el botón de plantillas
+app.set('view engine','.hbs')
 
 // Middlewares 
 app.use(express.urlencoded({extended:false}))
@@ -20,7 +37,7 @@ app.use(express.urlencoded({extended:false}))
 
 // Rutas 
 app.get('/',(req,res)=>{
-    res.send("Server on")
+    res.render('index')
 })
 
 // Archivos estáticos
